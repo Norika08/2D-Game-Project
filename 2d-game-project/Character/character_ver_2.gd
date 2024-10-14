16ef5a2df5df83cac85328035ac703190a1d5034
Norika08 <nkatsuya@sva.edu>
2024-10-14 17:48:49 -0400

    character moving

--- 2d-game-project/Character/character_ver_2.gd
+++ 2d-game-project/Character/character_ver_2.gd
@@ -1,25 +1,53 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+#const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprites = get_node("AnimatedSprite2D")
 
 func _physics_process(delta: float) -> void:
-	$AnimatedSprite2D.play("finish_fishing_left")
+	$AnimatedSprite2D.play("idling_right")
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		$AnimatedSprite2D.play("Walking_right")
 		velocity.x = direction * SPEED
+		#print(direction)
+		if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+		else: 
+			$AnimatedSprite2D.flip_h = false
+			
+	#var direction :=Input.get_axis("down","up")
+	#if direction:
+		#$AnimatedSprite2D.play("Walking_down")
+		#velocity.x = direction * SPEED
+		
+		#if direction == -1:
+			
+			
+	if Input.is_action_pressed("down"):
+		position.y += 500*delta
+	if Input.is_action_pressed("up"):
+		position.y -= 500*delta
+	if Input.is_action_pressed("left"):
+		position.x -= 500*delta
+	if Input.is_action_pressed("right"):
+		position.x += 500*delta
+		
+		
+	
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
