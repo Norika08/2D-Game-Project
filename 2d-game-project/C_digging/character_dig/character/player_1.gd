a4a2404219d503b1f5ff1d0ba969b51afcba663a
aimmmo <swen@sva.edu>
2024-10-14 02:40:36 -0400

    character
    Create character and it now can idle and walk in 4 different directions

---
+++ 2d-game-project/C_digging/character_dig/character/player_1.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+func _physics_process(_delta):
+	
+	var input_direction = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"),
+	)
+	
+	#print(input_direction)
+	
+	velocity = input_direction * move_speed
+	
+	move_and_slide()
+	#move_and_collide()
+	
