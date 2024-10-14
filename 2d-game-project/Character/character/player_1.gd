e3e91c136abc36802a98e68cabea3c0b35127641
Norika08 <nkatsuya@sva.edu>
2024-10-14 05:20:01 -0400

    Fishing movement

--- 2d-game-project/Character/character/player_1.gd
+++ 2d-game-project/Character/character/player_1.gd
@@ -7,6 +7,7 @@ func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"),
+		
 	)
 	
 	#print(input_direction)
